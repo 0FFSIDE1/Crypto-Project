03443a5e66e5eca1abe1ddcdd83659a92ce943ce
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-07-27 17:54:44 +0100

    regiseter new tables

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Plan, Transaction
+from .models import User, Plan, Transaction, Wallet
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Plan)
 admin.site.register(Transaction)
+admin.site.register(Wallet)
